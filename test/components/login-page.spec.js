5eb055910e96fbf880514efaef31cc2cde327852
Alexander Borovykh <immaculate.pine@gmail.com>
2016-08-10 18:30:47 +0200

    Fix login page spec to use proper API host

--- test/components/login-page.spec.js
+++ test/components/login-page.spec.js
@@ -2,13 +2,14 @@ import { expect } from 'chai'
 import React from 'react'
 import { mount } from 'enzyme'
 import LoginPage from '../../src/components/login-page'
+import config from '../../src/config'
 
 describe('components', () => {
   describe('LoginPage', () => {
     it('renders self and a login button', () => {
       const wrapper = mount(<LoginPage />)
       const button = wrapper.find('.btn-github')
-      expect(button.props().href).to.equal('http://localhost:3000/users/auth/github')
+      expect(button.props().href).to.equal(`${config.apiHost}/users/auth/github`)
     })
 
     it('changes body class', () => {
