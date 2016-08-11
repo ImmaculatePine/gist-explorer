899f368dd851ee9931e574b093bbeee9e6147b7c
Alexander Borovykh <immaculate.pine@gmail.com>
2016-08-11 10:54:08 +0200

    Slightly optimize webpack config and css size

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const NODE_ENV = process.env.NODE_ENV;
+
+let plugins = [
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+  }),
+];
+
+if (NODE_ENV === 'production') {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+    })
+  );
+};
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
@@ -13,13 +31,7 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/',
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-  ],
+  plugins: plugins,
   module: {
     preLoaders: [
       {
